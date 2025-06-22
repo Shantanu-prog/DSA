c4f6766cbaabf2e814958be7ac50c61841bdcb12
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-06-22 13:59:20 +0530

    PATTERN 4 CONCEPT

---
+++ pattern4/pateern4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "Enter the value for a: " << endl;
+    cin  >> a;
+
+    cout << "Enter the value for b: " << endl;
+    cin >> b;
+
+    if(a > b){
+        cout << a << " grater than " << b;
+    }
+    else {
+        cout << b << " grater than " << a;
+    }
+
+}
+
+ 
