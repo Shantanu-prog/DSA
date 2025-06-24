125af110056e4a5e11f12d2735c70dc83db1d4ad
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-06-24 13:50:16 +0530

    pateern4

--- pattern4/pateern4.cpp
+++ pattern4/pateern4.cpp
@@ -1,21 +1,19 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
+void printstar(int n){
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= i; j++){
+        cout << setw(2) << "*";
+    }
+    cout << endl;
+  }
+}
 int main(){
-    int a,b;
-    cout << "Enter the value for a: " << endl;
-    cin  >> a;
-
-    cout << "Enter the value for b: " << endl;
-    cin >> b;
-
-    if(a > b){
-        cout << a << " grater than " << b;
-    }
-    else {
-        cout << b << " grater than " << a;
-    }
-
+    int n;
+    cin >> n;
+    printstar(n);
+    return 0;
 }
 
  
