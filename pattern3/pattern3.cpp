8afa812d037b2caafcca15505fd07d1394e1e5da
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-06-24 13:34:24 +0530

    pattern3

--- pattern3/pattern3.cpp
+++ pattern3/pattern3.cpp
@@ -1,13 +1,20 @@
-#include <iostream>
+#include<iostream>
+#include<iomanip>
 using namespace std;
+void printnum(int n){
+   int count = 1;
+   for (int i = 1; i <= n; i++){
+      for(int j = 1; j <=n; j++){
+        cout << setw(3) << count << " ";
+        count = count + 1;
+      }
+      cout << endl;
+   }
+}
 
-int main() {
-  int a = 9, b = a+1;
-
-  if ((a = 10) == b){
-    cout << "a";
-  }
-  else {
-    cout << "b";
-  }  
-}   
+int main(){
+  int n;
+  cin >> n;
+  printnum(n);
+  return 0;
+}
