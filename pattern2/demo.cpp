ae6c91224a6d552422358b90cc2a82435bdd6a16
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-06-24 13:11:44 +0530

    Pattern 2

--- pattern2/demo.cpp
+++ pattern2/demo.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 using namespace std;
-void print1(int n){
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            cout << "* ";
+void print(int n){
+    int i = 1;
+      while(i <= n){
+        int j = 1;
+        while (j <= n)
+        {
+          cout << j << " ";
+          j++;  
         }
         cout << endl;
+        i++;
+      }
     }
-}
 int main() {
     int n;
     cin >> n;
-    print1(n);
+    print(n);
   return 0;
 }
