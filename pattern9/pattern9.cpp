881fcb1b2ad0adf187294ed0474a0c6f9b7063f5
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-06-25 12:17:09 +0530

    pattern9

--- pattern9/pattern9.cpp
+++ pattern9/pattern9.cpp
@@ -1,22 +1,50 @@
+// #include<iostream>
+// using namespace std;
+
+// void printchar(int n)
+// {
+//   for(int i = 1; i <=n; i++)
+//   {
+//     for(int j = 1; j <=n; j++)
+//     {   
+//         char ch = ('A' + i - 1);
+//         cout <<  ch << " ";
+//     }
+//     cout << endl;
+//   }
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     printchar(n);
+//     return 0;
+// }
+
 #include<iostream>
 using namespace std;
 
-void printchar(int n)
+void printalpha(int n)
 {
-  for(int i = 1; i <=n; i++)
-  {
-    for(int j = 1; j <=n; j++)
-    {   
+ int i = 1;
+ while (i <= n)
+ {
+    int j = 1;
+    while (j <= n)
+    {
         char ch = ('A' + i - 1);
-        cout <<  ch << " ";
+        cout << ch << " ";
+        j++;
     }
     cout << endl;
-  }
+    i++;
+ }
+ 
 }
 int main()
 {
     int n;
     cin >> n;
-    printchar(n);
+    printalpha(n);
     return 0;
 }
