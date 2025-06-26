ca41df12e4c953417cc4fdc643a9d4cafc62137a
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-06-26 10:46:52 +0530

    pattern13

---
+++ pattern13/pattern13.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+void printalpha(int n)
+{
+ int i = 1;
+ while (i <= n)
+ {
+    int j = 1;
+    while (j <= i)
+    {
+        char ch = ('A' + i - 1);
+        cout << ch << " ";
+        j++; 
+    }
+    cout << endl;
+    i++;
+ }
+ 
+}
+int main()
+{
+    int n;
+    cin >> n;
+    printalpha(n);
+    return 0;
+}
+
+                     //OR
+
+#include<iostream>
+using namespace std;
+
+void printchar(int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            char ch = ('A' + i - 1);
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+  int n;
+  cin >> n;
+  printchar(n);
+  return 0;
+}
