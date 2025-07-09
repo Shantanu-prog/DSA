75084ec609acdb6f08f2030dcba29d536b7292ba
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-07-09 15:40:03 +0530

    ..

---
+++ Cppbasics/switchStatmentsAndFunctions/calculator.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter the value of a" << '\n';
+    cin >> a;
+
+    cout << "Enter the value of b" << '\n';
+    cin >> b;
+
+    char op;
+    cout << "Enter the operation you want to perform" << endl;
+    cin >> op;
+
+    switch ( op )
+    {
+        case '+': cout << ( a + b ) << '\n';
+        break;
+
+        case '-': cout << ( a - b ) << '\n';
+        break;
+
+        case '*': cout << ( a * b ) << '\n';
+        break;
+
+        case '/': cout << ( a / b) << '\n';
+        break;
+
+        case '%': cout << (a % b) << '\n';
+        break;
+
+        default : cout << "Enter a valid number" << '\n';
+
+    }
+}
