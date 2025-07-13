d0570f8170cc46c2dbe27e33e23775acda4ffb25
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-07-13 11:34:02 +0530

    ..

---
+++ Array/Basic/ArrFunction.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[], int size)
+{
+    cout << "printing the array" << endl;
+
+    //print the array
+        for (int i = 0; i < size; i++)
+        {
+           cout << arr[i] << " ";
+        }
+         cout << endl; 
+         cout << " printing Done " << endl;
+    }
+    
+    int main()
+    {
+        //declare
+        int third[15] ={2,7};
+    
+        int n = 15;
+    
+        //accessing an array
+        printArray(third, 15);
+    
+        return 0;
+    }
