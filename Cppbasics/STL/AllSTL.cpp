cfbeab22a9461861ca5d53b295b9c8bc8643e1e0
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-07-29 12:30:55 +0530

    ..

--- Cppbasics/STL/AllSTL.cpp
+++ Cppbasics/STL/AllSTL.cpp
@@ -348,6 +348,57 @@ void MapExplained(){
 
      //erase, swap, size, empty, are same as above
 }
+void MultimapExplained(){
+    //everything same as map, only it can store multiple keys
+    //only mpp[key] cannot br used here
+    //store in shorted arrary
+    //works in log(n)
+}
+void UnorderedMapExplain(){
+    // same as set and unordered_Set difference.
+    //it is not shorted
+    //works in O(1)
+}
+
+bool comp(pair<int, int> p1, pair<int, int>p2){
+       if(p1.second < p2.second) return true;
+       if(p1.second > p2.second) return false;
+       //if they are same
+       if(p1.first > p2.first) return true;
+       return false;
+}
+
+void shortingExplained(){
+    sort(a, a + n);
+    sort(v.begin(), v.end());
+
+    sort(a + 2, a + 4);
+
+    sort(a, a + n, greater<int>);
+    pair <int, int> a[] = {{1, 2}, {2, 1}, {4, 1}};
+    // sort it according to second element
+    // if second element is same, then sort
+    // it according to first element but in descending
+
+    sort(a, a + n, comp);
+    // {4, 1}, {2, 1}, {1, 2}};
+
+    int num = 7;
+    int cnt = __builtin_popcount();
+
+    long long num = 12894783460982;
+    int cnt = __builtin_popcountll();
+
+    string s = "123";
+    sort(s.begin(), s.end());
+
+    do{
+        cout << s << endl;
+    }while(next_permutation(s.begin(), s. end()));
+    
+    int maxi = *max_element(a, a + n);
+}
+
 
 int main(){
     MapExplained();
