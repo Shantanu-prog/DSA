20cf968d2a876efdcb87c29a54a1e16184fdf814
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-08-28 17:44:32 +0530

    ..

--- practice/revese.cpp
+++ practice/revese.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int reverseNum (int n)
+{
+    int rev = 0;
+    while( n > 0)
+    {
+        int ld = n % 10;
+        n /= 10;
+        rev = ( rev * 10 ) + ld;
+    }
+    return rev;
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    int rev = 0;
-    while( n > 0 ){
-    int lsd = n % 10;
-    rev = ( rev * 10) + lsd;
-    n /= 10; 
-    }
-    cout << rev << 'n';
-    return 0;
+  int n;
+  cin >> n;
+  cout << reverseNum(n) << endl;
+  return 0;
 }
