5965b52d7a249c491bb9a002e23a9d3f2bb9eef3
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-09-02 16:32:28 +0530

    ..

--- practice/GCDorHCF.cpp
+++ practice/GCDorHCF.cpp
@@ -17,3 +17,30 @@ int main()
     cout << gcd(a, b) << endl;
     return 0;
 }
+
+     //or
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int GCD(int n1,int n2) {
+       
+       n1 = abs(n1);
+       n2 = abs(n2);
+
+       while( n2 != 0 )
+       {
+        int temp = n2;
+        n2 = n1 % n2;
+        n1 = temp;
+       }
+       return n1;
+    }
+int main()
+{
+    int n1, n2;
+    cout << "enter first number and second number : ";
+    cin >> n1 >> n2;
+    gcd(n1, n2);
+    return 0;
+}
