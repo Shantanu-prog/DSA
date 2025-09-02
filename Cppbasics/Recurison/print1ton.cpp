53117a0efee6e022cd0bf766d39a3cd5c48be138
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-09-02 18:14:16 +0530

    ..

---
+++ Cppbasics/Recurison/print1ton.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print1toN(int i, int n)
+{
+  if( i > n ) return;
+  cout << i << " ";
+  print1toN(i + 1, n);
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  print1toN(1, n);
+  return 0;
+}
