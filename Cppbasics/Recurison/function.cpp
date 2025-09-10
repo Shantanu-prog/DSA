704abc45e222b021b019195383062587ec0e2cbd
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-09-10 18:10:44 +0530

    ..

---
+++ Cppbasics/Recurison/function.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int functionalWay(int n)
+{
+  if( n == 0 )
+  {
+    return 0;
+  }
+  return n + functionalWay(n - 1);
+}
+int main()
+{
+  int n;
+  cin >> n;
+  cout << functionalWay(n) << endl;
+  return 0;
+}
