991a7957cd6663910d0beecaf4715dc9c79a7af7
Shantanu-prog <shantanukumar1nov@gmail.com>
2025-09-23 16:44:25 +0530

    ..

--- Cppbasics/Recurison/ReverseArray.cpp
+++ Cppbasics/Recurison/ReverseArray.cpp
@@ -16,3 +16,22 @@ int main(){
   for(int i = 0; i < n ; i++) cout << arr[i] << " ";
   return 0;
 }
+
+      //or
+
+void Reverse(int arr[], int l, int r)
+{
+  if (l >= r) return;
+  swap(arr[l], arr[r]);
+  Reverse(arr, l + 1, r - 1);
+}
+int main()
+{
+  int n;
+  cin >> n;
+  int arr[n];
+  for (int i = 0; i < n; i++) cin >> arr[i];
+  Reverse(arr, 0, n - 1);
+  for (int i = 0; i < n; i++) cout << arr[i] << " ";
+  return 0;
+}
