1f8a13c4749c301a20bb1faf820e824d4362f520
Shantanu-prog <shantanukumar1nov@gmail.com>
2026-01-20 16:29:07 +0530

    ..

--- practice/Revision.cpp
+++ practice/Revision.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 using namespace std;
 
-void pattern2(int n){
-   for(int i = 0; i <= n - 1; i++){
-    for(int j = 0; j <= i; j++){
-      cout << "* ";
-    } 
+void pattern3(int n){
+   for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= i; j++){
+      cout << j;
+    }
     cout << endl;
    }
 }
 int main(){
   int n;
   cin >> n;
-  pattern2(n);
+  pattern3(n);
   return 0;
 }
